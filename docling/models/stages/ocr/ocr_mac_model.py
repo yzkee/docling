455fcec8b299eaeda3547d2f8791b994eeba9087
Christoph Auer <60343111+cau-git@users.noreply.github.com>
2026-08-18 09:23:48 +0200

    fix: Add coordinate padding to OCRmac results (#4021)
    Signed-off-by: Christoph Auer <cau@zurich.ibm.com>

--- docling/models/stages/ocr/ocr_mac_model.py
+++ docling/models/stages/ocr/ocr_mac_model.py
@@ -108,10 +108,10 @@ class OcrMacModel(BaseOcrModel):
                             x2 = x1 + w * im_width
                             y1 = y2 - h * im_height
 
-                            left = x1 / self.scale
-                            top = y1 / self.scale
-                            right = x2 / self.scale
-                            bottom = y2 / self.scale
+                            left = x1 / self.scale + ocr_rect.l
+                            top = y1 / self.scale + ocr_rect.t
+                            right = x2 / self.scale + ocr_rect.l
+                            bottom = y2 / self.scale + ocr_rect.t
 
                             cells.append(
                                 TextCell(
